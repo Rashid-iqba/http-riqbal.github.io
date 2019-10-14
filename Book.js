7304bf65090894a2a21f16b9b46840d5bd41ed36
RASHID IQBAL <45360835+Rashid-iqba@users.noreply.github.com>
2019-10-14 10:46:04 +0530

    new entry

---
+++ Book.js
@@ -0,0 +1,7 @@
+function Search()
+{
+  
+
+	document.write("THE CONTENTS IS COMING SOON.......</hr>");
+	document.write("please wait...................................................................")
+}
--- index.js
+++
@@ -1,7 +0,0 @@
-function Search()
-{
-  
-
-	document.write("THE CONTENTS IS COMING SOON.......</hr>");
-	document.write("please wait...................................................................")
-}
